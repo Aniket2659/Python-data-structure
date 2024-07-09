497fd12f4cb3e889ea510e033c564947ed77e67f
Aniket2659 <aniket2659@gmail.com>
2024-07-09 23:01:37 +0530

    [aniket] add: program to find longest word in list

---
+++ string/5_longest_word.py
@@ -0,0 +1,11 @@
+def longest_word(li):
+    max=len(li[0])
+    for i in range(1,len(li)):
+        if len(li[i])>max:
+            max=len(li[i])
+    for j in li:
+        if len(j)==max:
+            a=j
+    return j
+given_li=['aniket','babar','ram','aakashhh']
+print(longest_word(given_li))
