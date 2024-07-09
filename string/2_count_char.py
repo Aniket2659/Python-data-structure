23a60a630d79fda8979966b601a11a2b3822a260
Aniket2659 <aniket2659@gmail.com>
2024-07-09 22:29:39 +0530

    [aniket] add: program to calculate count of character in string

---
+++ string/2_count_char.py
@@ -0,0 +1,7 @@
+def counting_char(string):
+    dict={}
+    for i in string:
+        dict[i]=string.count(i)
+    return dict
+given_string=input('enter a string :')
+print(counting_char(given_string))
