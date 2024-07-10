419ef57d781d57027f3e92cdb5b10cf9bd9f662a
Aniket2659 <aniket2659@gmail.com>
2024-07-10 12:09:36 +0530

    [aniket] add: program to sort the words

--- string/7_sorted_word.py
+++ string/7_sorted_word.py
@@ -2,6 +2,7 @@ def sorting(lst):
     unique_lst=set(lst)
     final_list=sorted(unique_lst)
     return ', '.join(final_list)
+
 words = input("Enter a comma separated sequence of words: ").strip().split(',')
 
 print(sorting(words))
