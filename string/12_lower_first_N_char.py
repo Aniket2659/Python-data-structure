33116812513e9b834819ebb4979374ceb494dea2
Aniket2659 <aniket2659@gmail.com>
2024-07-10 12:13:09 +0530

    [aniket] add: program to lower the first n char

--- string/12_lower_first_N_char.py
+++ string/12_lower_first_N_char.py
@@ -1,5 +1,6 @@
 def lower(string,N):
     return (string[0 : (N)].lower())+string[N:]
+
 string=input('enter string :')
 num=int(input('enter number :'))
 print(f'first {num} leters {string} converted into lower case :{lower(string,num)} ')
