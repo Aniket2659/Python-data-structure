0cfa819a1c596940aad466c0caf9a8e4365fd65a
Aniket2659 <aniket2659@gmail.com>
2024-07-10 12:10:45 +0530

    [aniket] add: program to format the text

--- string/9_formatted_text.py
+++ string/9_formatted_text.py
@@ -12,6 +12,7 @@ def formatted_text(string,width):
             para_len=len(j)+1
 
     return para
+
 string="""Before executing code, Python interpreter reads source file and define few special variables/global variables. If the python interpreter is running that module (the source file) as the main program, it sets the special __name__ variable to have a value “__main__”. If this file is being imported from another module, __name__ will be set to the module’s name. Module’s name is available as value to __name__ global variable."""
 print(formatted_text(string,50))
 
