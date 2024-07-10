f59a25975ad58134d2e27cdcff483cb8f62e7fe3
Aniket2659 <aniket2659@gmail.com>
2024-07-10 12:10:15 +0530

    [aniket] add: program to find sepecific part

--- string/8_specific_part.py
+++ string/8_specific_part.py
@@ -3,6 +3,7 @@ def last_part(string,spe_char):
     for i in string[::-1]:
         if i==spe_char:
             return string[string[::-1].index(i):]
+        
 string=input('enter a string')
 special_char=input(f'enter a special char in {string} :')
 
