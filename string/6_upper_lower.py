3a9854b92b0b6543e489c8e27bda554244e32b45
Aniket2659 <aniket2659@gmail.com>
2024-07-10 12:08:05 +0530

    [aniket] add: program to convert upper and lower case of strig

--- string/6_upper_lower.py
+++ string/6_upper_lower.py
@@ -2,6 +2,7 @@ def upper_lower_case(string):
     up=string.upper()
     low=string.lower()
     return up,low
+
 string=input("Enter a string :")
 up,low=upper_lower_case(string)
 print(f'upper case word = {up} and lower case word = {low}')    
