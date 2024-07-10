619c53d05f2939fb95165e41baa3fc4b1a99de52
Aniket2659 <aniket2659@gmail.com>
2024-07-10 12:29:46 +0530

    [aniket] add: program to sum of elements in lst

--- list/1_sum.py
+++ list/1_sum.py
@@ -3,6 +3,7 @@ def sum(list):
     for i in range(len(list)):
         sum=sum+list[i]
     return sum
+
 lst=list(map(int,input('enter :').split(',')))
 print(f'the sum of element in list is :{sum(lst)}')
         
