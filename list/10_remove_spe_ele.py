5cc5306adc8becf7bf7ac9871adaa972d9e0794b
Aniket2659 <aniket2659@gmail.com>
2024-07-11 15:03:59 +0530

    [aniket] add: program to remove specific element

--- list/10_remove_spe_ele.py
+++ list/10_remove_spe_ele.py
@@ -1,8 +1,9 @@
 def remover(lst):
+    new=[]
     for i in range(len(lst)):
-        if i==0 or i==4 or i==5:
-            lst.pop(i)
-    return lst
+        if i!=0 and i!=3 and i!=4:
+            new.append(lst[i])
+    return new
 
 list=['a','b','c','d','e','f','g']
 print(remover(list))
